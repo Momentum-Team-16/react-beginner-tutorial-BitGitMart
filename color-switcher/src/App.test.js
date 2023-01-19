dea2f9ec3cbb06c6025a538c834336dcd1df2752
Lucian Lodico <llodico@mac.com>
2023-01-19 14:24:57 -0500

    color picker

---
+++ color-switcher/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// A file where you can write automated tests for your App component. 
